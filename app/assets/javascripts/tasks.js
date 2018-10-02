ca72804b61f37916398f860838864ee8130c8cf7
wilbobraggins <wilbobraggins@gmail.com>
2018-10-02 22:36:28 +0000

    toggle checkbox strikethrough

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -6,7 +6,9 @@ $(function() {
     // usin <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id + '" class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -29,6 +31,11 @@ $(function() {
         task: {
           done: doneValue
         }
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
       });
     }
 
